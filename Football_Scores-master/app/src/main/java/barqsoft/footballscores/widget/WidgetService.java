55fbb7e6fa2445bfd0c8f786e758f6bfb145ea45
Sergei Zarochentsev <zsergey26@gmail.com>
2015-12-09 10:28:37 +0300

    Widget complete

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetService.java
@@ -5,10 +5,14 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
+import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
 
 /**
  * Created by ZSergei on 08.12.2015.
@@ -19,6 +23,31 @@ public class WidgetService extends IntentService{
         super("WidgetService");
     }
 
+    private static final String[] SCORES_COLUMNS = {
+            DatabaseContract.SCORES_TABLE + "." +
+                    DatabaseContract.scores_table._ID,
+            DatabaseContract.scores_table.DATE_COL,
+            DatabaseContract.scores_table.TIME_COL,
+            DatabaseContract.scores_table.HOME_COL,
+            DatabaseContract.scores_table.AWAY_COL,
+            DatabaseContract.scores_table.LEAGUE_COL,
+            DatabaseContract.scores_table.HOME_GOALS_COL,
+            DatabaseContract.scores_table.AWAY_GOALS_COL,
+            DatabaseContract.scores_table.MATCH_ID,
+            DatabaseContract.scores_table.MATCH_DAY
+    };
+
+    public static final int COL_HOME = 3;
+    public static final int COL_AWAY = 4;
+    public static final int COL_HOME_GOALS = 6;
+    public static final int COL_AWAY_GOALS = 7;
+    public static final int COL_DATE = 1;
+    public static final int COL_LEAGUE = 5;
+    public static final int COL_MATCHDAY = 9;
+    public static final int COL_ID = 8;
+    public static final int COL_MATCHTIME = 2;
+    public double detail_match_id = 0;
+
     @Override
     protected void onHandleIntent(Intent intent) {
 
@@ -26,21 +55,41 @@ public class WidgetService extends IntentService{
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
                 WidgetProvider.class));
 
+        Uri scoresUri = DatabaseContract.BASE_CONTENT_URI;
+        Cursor data = getContentResolver().query(scoresUri, SCORES_COLUMNS, null,
+                null, null);
+        if (data == null) {
+            return;
+        }
+        if (!data.moveToFirst()) {
+            data.close();
+            return;
+        }
+
+
+        String homeName = data.getString(COL_HOME);
+        String awayName = data.getString(COL_AWAY);
+        String matchTime = data.getString(COL_MATCHTIME);
+        String score = Utilies.getScores(data.getInt(COL_HOME_GOALS), data.getInt(COL_AWAY_GOALS));
+        int homeCrest = Utilies.getTeamCrestByTeamName(data.getString(COL_HOME));
+        int awayCrest = Utilies.getTeamCrestByTeamName(data.getString(COL_AWAY));
+        data.close();
+
         // Perform this loop procedure for each Today widget
         for (int appWidgetId : appWidgetIds) {
             int layoutId = R.layout.widget;
             RemoteViews views = new RemoteViews(getPackageName(), layoutId);
 
             // Add the data to the RemoteViews
-            views.setImageViewResource(R.id.widget_home_crest, R.drawable.manchester_united);
-            views.setTextViewText(R.id.widget_home_name, "Manchester United");
+            views.setImageViewResource(R.id.widget_home_crest, homeCrest);
+            views.setTextViewText(R.id.widget_home_name, homeName);
 
-            views.setImageViewResource(R.id.widget_away_crest, R.drawable.manchester_city);
-            views.setTextViewText(R.id.widget_away_name, "Manchester City");
+            views.setImageViewResource(R.id.widget_away_crest, awayCrest);
+            views.setTextViewText(R.id.widget_away_name, awayName);
 
-            views.setTextViewText(R.id.widget_score_textview, "2:4");
+            views.setTextViewText(R.id.widget_score_textview, score);
 
-            views.setTextViewText(R.id.widget_data_textview, "09.12.2015");
+            views.setTextViewText(R.id.widget_data_textview, matchTime);
 
             // Create an Intent to launch MainActivity
             Intent launchIntent = new Intent(this, MainActivity.class);
